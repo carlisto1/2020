5c0dbe156f11d59b6f50191efc6a0756eb1b84ef
edrees  abdalrhman <59860528+carlisto1@users.noreply.github.com>
2020-10-28 23:09:49 -0700

    student payment project
    student payment project
    include html and php and css files

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<form action="" method="POST">
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="about.css">
+</head>
+<body>
+	
+	<div class="bar">
+		<ul>
+			<li ><a href="index.php">Home</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li class="active"><a href="about.php">College Details</a></li>
+			<li><a href="Payment.php">Fees Payment</a></li>
+		</ul>
+		</div>
+
+	<table>
+
+
+<tr>
+	<td><p>How are we?</p> </td>
+	<td>
+<div><p>
+An international educational<br> 
+community that aims at<br>
+fostering an inclusive<br> 
+society in which every<br>
+member contributes to the<br> 
+common good through his/her<br>
+scientific skills and<br>
+personal commitment and values</p>
+</div>
+</td>
+<td><p>our goal</p></td>
+<td><div><p>
+college of Science and<br> 
+Technology is a Sudanese and<br>
+international educational<br>
+community that considers every<br>
+human being as a free person<br>
+capable of building his/her <br>
+own history, orientated towards<br>
+excellency, both spiritually and <br>
+professionally.
+
+</div>
+</td>
+
+<td><p>our vision</p></td>
+<td><div><p>
+College of Science and<br>
+Technology is an international<br>
+educational community that<br>
+aims at fostering an inclusive<br>
+society in which every member<br>
+contributes to the common good<br>
+through his/her scientific<br>
+skills and personal<br> 
+commitment and values.<br>
+</div>
+</td>
+</tr>
+</table>
+
+
+</body>
+</form>
+</html>
---
+++ contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<form action="holdcontact.php" method="POST">
+<head>
+
+	<br><br><br><br><br><br><br><br><br>
+	<title>contact us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+	<link rel="stylesheet" type="text/css" href="contact.css">
+</head>
+<body>
+	<div class="bar">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li class="active"><a href="contact.php">Contact</a></li>
+      <li><a href="about.php">College Details</a></li>
+      <li><a href="Payment.php">Fees Payment</a></li>
+    </ul>
+    </div>
+    <br>
+<div calss="con">
+<br><br><br><br><br><br>
+<br><br><br><br><br><br>
+	<h1>Contact Us</h1>
+
+ <div class="contact-info">
+      <div class="card">
+        <i class="card-icon far fa-envelope"></i>
+        <p>college@domain.com</p>
+      </div>
+
+      <div class="card">
+        <i class="card-icon fas fa-phone"></i>
+        <p>+249922334550</p>
+      </div>
+
+      <div class="card">
+        <i class="card-icon fas fa-map-marker-alt"></i>
+        <p>khrtoum-omderman</p>
+      </div>
+    </div>
+</body>
+</form>
+</html>
